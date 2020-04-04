cfb6f63921af12c31186a1895393d63ef3dc7ad6
yulinzhihou <yulinzhihou@163.com>
2020-04-05 00:26:30 +0800

    404 获取用户openid再跳转商品页面。测试-89

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -118,7 +118,6 @@ class Frontend extends Controller
             $weChat = new Oauth($this->weChatConfig);
             // 执行操作
             $result = $weChat->getOauthRedirect($redirect_url);
-            dump($result);die;
         } catch (\Exception $e){
             // 异常处理
             echo  $e->getMessage();
