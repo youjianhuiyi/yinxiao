73af7e84ceac85f5b42aa1fdb136513570cc3a0f
yulinzhihou <yulinzhihou@163.com>
2020-04-05 19:49:14 +0800

    405 订单调整。调整获取微信授权的结构逻辑

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -30,9 +30,8 @@ class Index extends Frontend
     {
         //判断访问链接，如果有微信授权链接参数，直接放行到落地页面。如果没有则进行微信授权认证
         $params = $this->request->param();
-        if (!isset($params['code'])) {
-            $this->intoBefore();
-        } else {
+        if (isset($params['code']) && !empty($params['code'])) {
+
             try {
                 if (!Cache::has('pay_info_' . $params['tid'])) {
                     //设置缓存-本次记录好缓存，判断是否是支付配置信息记录
@@ -45,24 +44,30 @@ class Index extends Frontend
                 // 实例接口
                 $weChat = new Oauth($this->weChatConfig);
                 // 执行操作
-                $result = $weChat->getOauthAccessToken();
+                $wxUserInfo = $weChat->getOauthAccessToken();
+
+                //pay_domain_1缓存，记录支付域名，和支付信息一起，记录当前访问用户与固定一个支付域名绑定，30分钟。
+
+                //表示已经获取了openid
+                //判断链接是否有效
+                //TODO::判断逻辑暂时不体现，后期可以使用全局方法进行调用检测
+                $data = [
+                    'openid'    => $wxUserInfo['openid'],/*openid*/
+                    'pay_domain'=> 'http://pay.ckjdsak.cn/',/*支付域名*/
+                ];
+                $this->assign('data',$data);
+                return $this->view->fetch('shoes');
+
             } catch (\Exception $e) {
                 // 异常处理
                 echo $e->getMessage();
             }
+
+        } else {
+
+            $this->intoBefore();
         }
 
-        //pay_domain_1缓存，记录支付域名，和支付信息一起，记录当前访问用户与固定一个支付域名绑定，30分钟。
-
-        //表示已经获取了openid
-        //判断链接是否有效
-        //TODO::判断逻辑暂时不体现，后期可以使用全局方法进行调用检测
-        $data = [
-            'openid'    => $result['openid'],/*openid*/
-            'pay_domain'=> 'http://pay.ckjdsak.cn/',/*支付域名*/
-        ];
-        $this->assign('data',$data);
-        return $this->view->fetch('shoes');
     }
 
 }
