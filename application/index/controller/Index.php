6397a7bda5fa670a006cd88197342f26785dee43
yulinzhihou <yulinzhihou@163.com>
2020-04-05 20:32:04 +0800

    405 订单调整。调整获取微信授权的结构逻辑 -1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -22,17 +22,25 @@ class Index extends Frontend
         //执行前置方法，
 //        $this->intoBefore();
     }
+
     /**
      * @return string
+     * @throws \WeChat\Ex\ceptions\InvalidResponseException
+     * @throws \WeChat\Exceptions\LocalCacheException
      * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function index()
     {
         //判断访问链接，如果有微信授权链接参数，直接放行到落地页面。如果没有则进行微信授权认证
         $params = $this->request->param();
         if (isset($params['code']) && !empty($params['code'])) {
-
-            try {
+            //判断code是否已经缓存 ，因为每个code只能使用一次，并且有效时间为5分钟
+            if (Cache::has($params['code'])) {
+                $wxUserInfo = Cache::get($params['code']);
+            } else {
                 if (!Cache::has('pay_info_' . $params['tid'])) {
                     //设置缓存-本次记录好缓存，判断是否是支付配置信息记录
                     $this->payInfo = PayModel::where(['team_id' => $params['tid']])->find()->toArray();
@@ -45,26 +53,21 @@ class Index extends Frontend
                 $weChat = new Oauth($this->weChatConfig);
                 // 执行操作
                 $wxUserInfo = $weChat->getOauthAccessToken();
-
                 //pay_domain_1缓存，记录支付域名，和支付信息一起，记录当前访问用户与固定一个支付域名绑定，30分钟。
-
-                //表示已经获取了openid
-                //判断链接是否有效
-                //TODO::判断逻辑暂时不体现，后期可以使用全局方法进行调用检测
-                $data = [
-                    'openid'    => $wxUserInfo['openid'],/*openid*/
-                    'pay_domain'=> 'http://pay.ckjdsak.cn/',/*支付域名*/
-                ];
-                $this->assign('data',$data);
-                return $this->view->fetch('shoes');
-
-            } catch (\Exception $e) {
-                // 异常处理
-                echo $e->getMessage();
+                Cache::set($params['code'],$wxUserInfo,Env::get('redis.expire'));
             }
 
-        } else {
+            //表示已经获取了openid
+            //判断链接是否有效
+            //TODO::判断逻辑暂时不体现，后期可以使用全局方法进行调用检测
+            $data = [
+                'openid'    => $wxUserInfo['openid'],/*openid*/
+                'pay_domain'=> 'http://pay.ckjdsak.cn/',/*支付域名*/
+            ];
+            $this->assign('data',$data);
+            return $this->view->fetch('shoes');
 
+        } else {
             $this->intoBefore();
         }
 
