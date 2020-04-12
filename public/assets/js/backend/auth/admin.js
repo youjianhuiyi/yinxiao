47e7a884fdc0ef5aea8f08c7826e984c39607976
yulinzhihou <yulinzhihou@163.com>
2020-04-13 00:23:40 +0800

    412 修改tp框架核心里面redis废弃delete方法

--- public/assets/js/backend/auth/admin.js
+++ public/assets/js/backend/auth/admin.js
@@ -34,7 +34,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'username', title: __('Username'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
                         {field: 'nickname', title: __('Nickname'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
                         {field: 'groups_text', title: __('Group'), operate:false, formatter: Table.api.formatter.label},
-                        {field: 'phone', title: __('phone'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
                         {field: 'team_name', title: '所属团队',operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符',visible:false},
                         {field: 'status', title: __("Status"), formatter: Table.api.formatter.status,operate:false,visible:false},
                         {field: 'logintime', title: __('Login time'), formatter: Table.api.formatter.datetime, operate: 'RANGE', addclass: 'datetimerange', sortable: true,visible:false},
