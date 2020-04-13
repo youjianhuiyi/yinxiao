3ac6e0cf8d14d0f080fbd9a4666820d689a7d4cf
yulinzhihou <yulinzhihou@163.com>
2020-04-13 11:35:23 +0800

    413 允许403跨域请求接口

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -89,6 +89,11 @@ class Index extends Frontend
      */
     public function loadGround()
     {
+        header('Content-Type: text/html;charset=utf-8');
+        header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+        header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
+        header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
+        header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin'); // 设置允许自定义请求头的字段
         $remote = $this->request->host();
         Cache::set('remote',$remote);
         //接收403页面来的参数请求
