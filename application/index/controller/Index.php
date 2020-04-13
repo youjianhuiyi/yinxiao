7cb10413a440e44ef8725ab9a78230c24e6966e7
yulinzhihou <yulinzhihou@163.com>
2020-04-13 14:02:20 +0800

    413 403接口请求允许所有域名-4

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -126,20 +126,20 @@ class Index extends Frontend
                     $luckDomain = 'http://www.qq.com';
                 }
                 $wholeDomain = 'http://'.time().'.'.$luckDomain.'/index.php/index/index?';
-                return ['code'=>'successcode','data'=>$wholeDomain.$queryStr];
+                return json(['code'=>'successcode','data'=>$wholeDomain.$queryStr]);
             } else {
                 //表示验证失败
-                return ['code'=>'failure','data'=>'http://www.qq.com'];
+                return json(['code'=>'failure','data'=>'http://www.qq.com']);
             }
         } else {
             //缓存数据不存在了。需要查找数据表
             $urlData = $this->urlModel->where(['check_code'=>$params['code']])->find();
             if ($urlData) {
                 //表示验证成功，
-                return ['code'=>'successcode','data'=>'http://www.baidu.com'];
+                return json(['code'=>'successcode','data'=>'http://www.baidu.com']);
             } else {
                 //表示验证失败
-                return ['code'=>'failure','data'=>'http://www.qq.com'];
+                return json(['code'=>'failure','data'=>'http://www.qq.com']);
             }
         }
 
