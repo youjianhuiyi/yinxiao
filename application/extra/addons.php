358daa084abc84718f089176a018706ee1f6d666
yulinzhihou <yulinzhihou@163.com>
2020-04-15 00:20:38 +0800

    414 完成背景更换

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,14 +4,8 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
-    'admin_login_init' => 
-    array (
-      0 => 'loginbg',
-    ),
   ),
   'route' => 
   array (
-    '/qrcode$' => 'qrcode/index/index',
-    '/qrcode/build$' => 'qrcode/index/build',
   ),
 );
