95cf1e5a4919368eba8b5a5bee9a6f1a36693f63
yulinzhihou <yulinzhihou@163.com>
2020-04-15 21:14:56 +0800

    415 完成享钱支付curd功能

---
+++ application/admin/lang/zh-cn/sysconfig/xpay.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Team_id'    => '团队ID',
+    'Team_name'  => '团队名称',
+    'Pay_name'   => '支付名称',
+    'Mch_code'   => '商户号',
+    'Mch_key'    => '商户密钥',
+    'Api_url'    => '接口地址',
+    'Createtime' => '增加时间',
+    'Updatetime' => '更新时间',
+    'Deletetime' => '删除时间'
+];
---
+++ public/assets/js/backend/sysconfig/xpay.js
@@ -0,0 +1,117 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'sysconfig/xpay/index' + location.search,
+                    add_url: 'sysconfig/xpay/add',
+                    edit_url: 'sysconfig/xpay/edit',
+                    del_url: 'sysconfig/xpay/del',
+                    multi_url: 'sysconfig/xpay/multi',
+                    table: 'xpay',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                search:false,
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id'),operate:false},
+                        {field: 'team_id', title: __('Team_id'),operate:false,visible:false},
+                        {field: 'team_name', title: __('Team_name'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符',visible:false},
+                        {field: 'pay_name', title: __('Pay_name'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
+                        {field: 'mch_code', title: __('Mch_code'),operate:false},
+                        {field: 'mch_key', title: __('Mch_key'),operate: false},
+                        {field: 'api_url', title: __('Api_url'), formatter: Table.api.formatter.url,operate: false},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime,visible: false},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        recyclebin: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    'dragsort_url': ''
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: 'sysconfig/xpay/recyclebin' + location.search,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {
+                            field: 'deletetime',
+                            title: __('Deletetime'),
+                            operate: 'RANGE',
+                            addclass: 'datetimerange',
+                            formatter: Table.api.formatter.datetime
+                        },
+                        {
+                            field: 'operate',
+                            width: '130px',
+                            title: __('Operate'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            buttons: [
+                                {
+                                    name: 'Restore',
+                                    text: __('Restore'),
+                                    classname: 'btn btn-xs btn-info btn-ajax btn-restoreit',
+                                    icon: 'fa fa-rotate-left',
+                                    url: 'sysconfig/xpay/restore',
+                                    refresh: true
+                                },
+                                {
+                                    name: 'Destroy',
+                                    text: __('Destroy'),
+                                    classname: 'btn btn-xs btn-danger btn-ajax btn-destroyit',
+                                    icon: 'fa fa-times',
+                                    url: 'sysconfig/xpay/destroy',
+                                    refresh: true
+                                }
+                            ],
+                            formatter: Table.api.formatter.operate
+                        }
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
