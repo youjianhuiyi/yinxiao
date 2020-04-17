0d3aac7030e9ee6a01fce48f9c8f172c0c279a29
yulinzhihou <yulinzhihou@163.com>
2020-04-17 23:32:56 +0800

    417 初始化报表页面10%

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -3,8 +3,7 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
-use think\Config;
-
+use app\admin\model\order\Order as OrderModel;
 /**
  * 数据报表
  *
@@ -14,41 +13,33 @@ use think\Config;
 class Dashboard extends Backend
 {
 
+    protected $orderModel = null;
+
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->orderModel = new OrderModel();
+    }
+
     /**
      * 查看
      */
     public function index()
     {
-        $seventtime = \fast\Date::unixtime('day', -7);
-        $paylist = $createlist = [];
-        for ($i = 0; $i < 7; $i++)
-        {
-            $day = date("Y-m-d", $seventtime + ($i * 86400));
-            $createlist[$day] = mt_rand(20, 200);
-            $paylist[$day] = mt_rand(1, mt_rand(1, $createlist[$day]));
+        $userInfo = $this->adminInfo;
+        if ($userInfo['id'] == 1) {
+            //表示是平台总管理员，可以查看所有记录
+            $orderData = collection($this->orderModel->select());
+        } elseif ($userInfo['pid'] == 0 && $userInfo['id'] != 1) {
+            //老板查看团队所有人员的数据
+            $orderData = collection($this->orderModel->where(['team_id'=>$this->adminInfo['team_id']])->select());
+        } elseif ($userInfo['pid'] != 0 && $userInfo['level'] != 2) {
+            //组长查看自己及以下员工的数据
+            $orderData = collection($this->orderModel->where(['team_id'=>$this->adminInfo['team_id']])->select());
+        } else {
+            //业务员只能查看自己的订单数据
+            $orderData = collection($this->orderModel->where(['admin_id'=>$this->adminInfo['id']])->select());
         }
-        $hooks = config('addons.hooks');
-        $uploadmode = isset($hooks['upload_config_init']) && $hooks['upload_config_init'] ? implode(',', $hooks['upload_config_init']) : 'local';
-        $addonComposerCfg = ROOT_PATH . '/vendor/karsonzhang/fastadmin-addons/composer.json';
-        Config::parse($addonComposerCfg, "json", "composer");
-        $config = Config::get("composer");
-        $addonVersion = isset($config['version']) ? $config['version'] : __('Unknown');
-        $this->view->assign([
-            'totaluser'        => 35200,
-            'totalviews'       => 219390,
-            'totalorder'       => 32143,
-            'totalorderamount' => 174800,
-            'todayuserlogin'   => 321,
-            'todayusersignup'  => 430,
-            'todayorder'       => 2324,
-            'unsettleorder'    => 132,
-            'sevendnu'         => '80%',
-            'sevendau'         => '32%',
-            'paylist'          => $paylist,
-            'createlist'       => $createlist,
-            'addonversion'       => $addonVersion,
-            'uploadmode'       => $uploadmode
-        ]);
 
         return $this->view->fetch();
     }
