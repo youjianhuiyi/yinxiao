b299a58aa70b9bcae4fbfb779f97646855f259ef
yulinzhihou <yulinzhihou@163.com>
2020-04-18 23:33:35 +0800

    418 回调验签失败-1

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -318,6 +318,7 @@ class Frontend extends Controller
         } else {
             return false;
         }
+        Cache::set('sign_str',$string);
         return strtoupper(md5(ltrim($string,'&')));/*执行加密算法*/
     }
 
