80b3fd033b65dc8bfe70c9831c9a6e17c03af7fc
yulinzhihou <yulinzhihou@163.com>
2020-04-19 21:57:55 +0800

    419 修改支付参数配置的问题

--- application/admin/controller/sysconfig/Pay.php
+++ application/admin/controller/sysconfig/Pay.php
@@ -193,7 +193,7 @@ class Pay extends Backend
             $params = $this->request->post("row/a");
             //如果有提交team_id，表示是编辑 操作，否则是x-editable操作。
             if (isset($params['team_id'])) {
-                $teamName = $this->teamModel->where('id',$params['team_id'])->find()['name'];
+                $teamName = $this->teamModel->where('id',$this->adminInfo['team_id'])->find()['name'];
                 $params['team_name'] = $teamName ? $teamName :'未知团队';
             }
             if ($params) {
