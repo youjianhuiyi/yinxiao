1a3a801cf609e7833af08332c31eb53becc4b6a5
yulinzhihou <yulinzhihou@163.com>
2020-04-20 15:43:42 +0800

    420 支付回调验证问题-4

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -256,6 +256,7 @@ class Frontend extends Controller
         }
         //最后拼接商户号入网的reqKey参数
         $string .= '&key='.$MchKey;
+        Cache::set('x-string',ltrim($string,'&'));
         /*执行加密算法*/
         return strtoupper(md5(ltrim($string,'&')));
     }
