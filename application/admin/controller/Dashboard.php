8d13bbe208c5f6e9f3725a50a9fffdbd53bba2f6
yulinzhihou <yulinzhihou@163.com>
2020-04-22 13:10:20 +0800

    422 数据报表功能基本完成。

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use app\common\controller\Backend;
 use app\admin\model\order\Order as OrderModel;
+use app\admin\model\team\Team as TeamModel;
 use app\admin\model\data\Visit as VisitModel;
 use app\admin\model\production\url as UrlModel;
 use app\admin\model\Admin as AdminModel;
@@ -22,6 +23,7 @@ class Dashboard extends Backend
     protected $visitModel = null;
     protected $urlModel = null;
     protected $adminModel = null;
+    protected $teamModel = null;
 
     public function _initialize()
     {
@@ -30,8 +32,8 @@ class Dashboard extends Backend
         $this->urlModel = new UrlModel();
         $this->visitModel = new VisitModel();
         $this->adminModel = new AdminModel();
+        $this->teamModel = new TeamModel();
     }
-
 
     /**
      * 获取当天0点到当天23点59分59秒的时间戳
@@ -45,6 +47,30 @@ class Dashboard extends Backend
     }
 
     /**
+     * @description:根据数据
+     * @param {dataArr:需要分组的数据；keyStr:分组依据}
+     * @return array
+     */
+    protected function dataGroup($dataArr, $keyStr,$userIds)
+    {
+        $newArr=[];
+        foreach ($dataArr as $k => $val) {    //数据根据日期分组
+            $newArr[$val[$keyStr]][] = $val;
+        }
+        foreach ($newArr as $key => $value) {
+            $newArr[$key] = count($value);
+        }
+
+        $adminIds = array_keys($newArr);
+        foreach ($userIds as $userId) {
+            if (!in_array($userId,$adminIds)) {
+                $newArr[$userId] = 0;
+            }
+        }
+        return $newArr;
+    }
+
+    /**
      * 查看
      */
     public function index()
@@ -52,26 +78,63 @@ class Dashboard extends Backend
         //获取当前用户信息
         $userInfo = $this->adminInfo;
         $timeData = $this->getBeginEndTime();
+        $teamData = $this->teamModel->column('name','id');
+        $adminName = $this->adminModel->column('nickname','id');
+        $userIds = $this->getUserLower();
+//        dump($userIds);die;
 
         if ($userInfo['id'] == 1) {
             //表示是平台总管理员，可以查看所有记录
-            $userData = collection($this->adminModel->select())->toArray();
+            $userInfoData = collection($this->adminModel->select())->toArray();
             //获取当天时间 0点到23点59分59秒的订单数量。
-            $orderDoneData = collection($this->orderModel->where('updatetime','>',$timeData[0])->where('updatetime','<',$timeData[1])->select())->toArray();
-            $visitData = collection($this->visitModel->select())->toArray();
+            $orderDoneData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('pay_status',1)
+                ->select();
+            $orderDoneData = collection($orderDoneData)->toArray();
+            $orderDoneData = $this->dataGroup($orderDoneData,'admin_id',$userIds);
+
+            $orderData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->select();
+            $orderData = collection($orderData)->toArray();
+            $orderData = $this->dataGroup($orderData,'admin_id',$userIds);
+
+            $visitData = $this->visitModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->select();
+            $visitData = collection($visitData)->toArray();
+            $visitData = $this->dataGroup($visitData,'admin_id',$userIds);
+//            dump($userIds);die;
+//            dump($visitData);die;
 
         } elseif ($userInfo['pid'] == 0 && $userInfo['id'] != 1) {
             //老板查看团队所有人员的数据
             //获取团队下所有的用户数据
-            $uesrData = $this->adminModel->where('team_id',$userInfo['team_id'])->select();
-            $userData  = collection($uesrData)->toArray();
+            $userInfoData = $this->adminModel->where('team_id',$userInfo['team_id'])->select();
+            $userInfoData  = collection($userInfoData)->toArray();
             //获取当前此团队下订单的所有数量
-            $orderDoneData = $this->orderModel
+            $orderData = $this->orderModel
                 ->where('updatetime','>',$timeData[0])
                 ->where('updatetime','<',$timeData[1])
                 ->where('team_id',$userInfo['team_id'])
                 ->select();
+            $orderData = collection($orderData)->toArray();
+            $orderData = $this->dataGroup($orderData,'admin_id',$userIds);
+
+            //获取当前此团队成交订单的所有数量
+            $orderDoneData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('team_id',$userInfo['team_id'])
+                ->where('pay_status',1)
+                ->select();
             $orderDoneData = collection($orderDoneData)->toArray();
+            $orderDoneData = $this->dataGroup($orderDoneData,'admin_id',$userIds);
+
             //获取当前团队下所有用户的访问数量
             $visitData = $this->visitModel
                 ->where('updatetime','>',$timeData[0])
@@ -79,25 +142,95 @@ class Dashboard extends Backend
                 ->where('team_id',$userInfo['team_id'])
                 ->select();
             $visitData = collection($visitData)->toArray();
+            $visitData = $this->dataGroup($visitData,'admin_id',$userIds);
 
         } elseif ($userInfo['pid'] != 0 && $userInfo['level'] != 2) {
             //组长查看自己及以下员工的数据
-            $lowerUser = $this->getUserLower();
-            $orderData = collection($this->orderModel->where(['team_id'=>$this->adminInfo['team_id']])->select())->toArray();
+            $userInfoData = $this->adminModel
+                ->where('team_id',$userInfo['team_id'])
+                ->where('id','in',$userIds)
+                ->select();
+            $userInfoData  = collection($userInfoData)->toArray();
+            //订单下单量
+            $orderData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('team_id',$userInfo['team_id'])
+                ->where('admin_id','in',$userIds)
+                ->select();
+            $orderData = collection($orderData)->toArray();
+            $orderData = $this->dataGroup($orderData,'admin_id',$userIds);
+
+            //获取当前此团队成交订单的所有数量
+            $orderDoneData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('team_id',$userInfo['team_id'])
+                ->where('admin_id','in',$userIds)
+                ->where('pay_status',1)
+                ->select();
+            $orderDoneData = collection($orderDoneData)->toArray();
+            $orderDoneData = $this->dataGroup($orderDoneData,'admin_id',$userIds);
+
+            //获取当前团队下所有用户的访问数量
+            $visitData = $this->visitModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('team_id',$userInfo['team_id'])
+                ->where('admin_id','in',$userIds)
+                ->select();
+            $visitData = collection($visitData)->toArray();
+            $visitData = $this->dataGroup($visitData,'admin_id',$userIds);
+
         } else {
             //业务员只能查看自己的订单数据
-            $lowerUser = $this->getUserLower();
-            $orderData = collection($this->orderModel->where(['admin_id'=>$this->adminInfo['id']])->select())->toArray();
+            //获取当前此团队下订单的所有数量
+            $userInfoData = $this->adminModel
+                ->where('team_id',$userInfo['team_id'])
+                ->where('id',$userIds[0])
+                ->select();
+            $userInfoData  = collection($userInfoData)->toArray();
+
+            $orderData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('id',$userInfo['id'])
+                ->select();
+            $orderData = collection($orderData)->toArray();
+            $orderData = $this->dataGroup($orderData,'admin_id',$userIds);
+
+            //获取当前此团队成交订单的所有数量
+            $orderDoneData = $this->orderModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('admin_id',$userInfo['id'])
+                ->where('pay_status',1)
+                ->select();
+            $orderDoneData = collection($orderDoneData)->toArray();
+            $orderDoneData = $this->dataGroup($orderDoneData,'admin_id',$userIds);
+
+            //获取当前团队下所有用户的访问数量
+            $visitData = $this->visitModel
+                ->where('updatetime','>',$timeData[0])
+                ->where('updatetime','<',$timeData[1])
+                ->where('admin_id',$userInfo['id'])
+                ->select();
+            $visitData = collection($visitData)->toArray();
+            $visitData = $this->dataGroup($visitData,'admin_id',$userIds);
         }
 
-        $this->assign('visitData',$visitData);
-        $this->assign('userData',$userData);
+        $this->assign('orderDoneData',$orderDoneData);/*成交数量*/
+        $this->assign('orderData',$orderData);/*下单数量*/
+        $this->assign('visitData',$visitData);/*访问数量*/
+        $this->assign('userInfoData',$userInfoData);/*用户数据*/
+        $this->assign('teamData',$teamData);/*团队数据*/
+        $this->assign('adminName',$adminName);/*团队数据*/
         return $this->view->fetch();
     }
 
 
     /**
-     * 获取用户关系。往下获取，
+     * 获取用户关系。往
      * @return array
      * @throws \think\db\exception\DataNotFoundException
      * @throws \think\db\exception\ModelNotFoundException
@@ -106,29 +239,33 @@ class Dashboard extends Backend
     public function getUserLower()
     {
         if ($this->adminInfo['id'] == 1) {
-            //表示当前为平台管理员
-            $data = collection(\app\admin\model\Admin::field(['id','pid','nickname'])->order('id desc')->select())->toArray();
-        } elseif ($this->adminInfo['pid'] == 0 && $this->adminInfo['id'] !=1) {
-            //表示当前用户为老板用户，只能查看到自己团队下的所有用户
-            $data = collection(\app\admin\model\Admin::field(['id','pid','nickname'])->where('team_id',$this->adminInfo['team_id'])->order('id desc')->select())->toArray();
+            $data = $this->adminModel->field(['id','pid','nickname'])->order('id desc')->select();
+            $data = collection($data)->toArray();
+        } elseif ($this->adminInfo['pid'] == 0) {
+            $data = $this->adminModel->field(['id','pid','nickname'])->where('team_id',$this->adminInfo['team_id'])->order('id desc')->select();
+            $data = collection($data)->toArray();
+        } elseif ($this->adminInfo['pid'] != 0 && $this->adminInfo['level'] != 2) {
+            $data = $this->adminModel->field(['id','pid','nickname'])->where('team_id',$this->adminInfo['team_id'])->order('id desc')->select();
+            $data = collection($data)->toArray();
         } else {
-            //表示是团队下组长级的权限
-            $data = [
-                $this->adminInfo['id'],
-            ];
-        }
-        if ($this->adminInfo['pid'] == 0 || $this->adminInfo['id'] == 1) {
-            $tree = Tree::instance();
-            $tree->init($data,'pid');
-            $teamList = $tree->getTreeList($tree->getTreeArray(0), 'nickname');
-            $adminData = [];
-            foreach ($teamList as $k => $v) {
-                $adminData[] = $v['id'];
-            }
-        } else {
-            $adminData = $data;
+            $data = $this->adminModel->field(['id','pid','nickname'])->find($this->adminInfo['id']);
         }
 
+        $tree = Tree::instance();
+        $tree->init($data,'pid');
+        if ($this->adminInfo['id'] == 1) {
+            $teamList = $tree->getTreeList($tree->getTreeArray(0), 'nickname');
+        } elseif ($this->adminInfo['pid'] == 0) {
+            $teamList = $tree->getTreeList($tree->getTreeArray(0), 'nickname');
+        } else {
+            $teamList = $tree->getTreeList($tree->getTreeArray($this->adminInfo['id']), 'nickname');
+        }
+        $adminData = [];
+        foreach ($teamList as $k => $v) {
+            $adminData[] = $v['id'];
+        }
+        //把自己添加进去
+        array_push($adminData,$this->adminInfo['id']);
         return $adminData;
     }
 
