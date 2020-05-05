25d19ff20500d0278160d0cd1444e103e1fdf134
yulinzhihou <yulinzhihou@163.com>
2020-05-05 13:08:15 +0800

    5-5 完成测试商户的功能

--- application/admin/controller/sysconfig/Xpay.php
+++ application/admin/controller/sysconfig/Xpay.php
@@ -4,6 +4,7 @@ namespace app\admin\controller\sysconfig;
 
 use app\admin\model\data\PayRecord as PayRecordModel;
 use app\common\controller\Backend;
+use think\Cache;
 use think\Db;
 use think\exception\PDOException;
 use think\exception\ValidateException;
@@ -12,6 +13,7 @@ use app\admin\model\order\Order as OrderModel;
 use app\admin\model\sysconfig\Xpay as XpayModel;
 use app\admin\model\Admin as AdminModel;
 use app\admin\model\sysconfig\Payset as PaySetModel;
+use app\admin\model\production\Url as UrlModel;
 
 
 /**
@@ -33,6 +35,7 @@ class Xpay extends Backend
     protected $orderModel = null;
     protected $payRecordMode = null;
     protected $paysetModel = null;
+    protected $urlModel = null;
 
     public function _initialize()
     {
@@ -43,6 +46,7 @@ class Xpay extends Backend
         $this->orderModel = new OrderModel();
         $this->payRecordMode = new PayRecordModel();
         $this->paysetModel = new PaySetModel();
+        $this->urlModel = new UrlModel();
 
         //团队数据
         if ($this->request->action() == 'add' || $this->request->action() == 'edit' || $this->request->action() == 'index' ) {
@@ -320,6 +324,10 @@ class Xpay extends Backend
 
         $params = $this->request->param();
         $payInfo = $this->model->get($params['pay_id']);
+        //缓存支付数据用于回调时使用
+//        $payInfo = Cache::get($orderInfo['order_ip'].'-'.$checkCode.'-xpay_config');
+        $url = $this->urlModel->where('admin_id',$this->adminInfo['id'])->find();
+        Cache::set($this->request->ip().'-'.$url['check_code'].'-xpay_config',$payInfo,1800);
         $teamData = $this->teamModel->get($params['tid']);
         $goodsName = '测试支付通道商品'.$payInfo['pay_name'];
 
@@ -336,7 +344,7 @@ class Xpay extends Backend
             'team_name' => '测试订单数据',
             'production_id'     => 4,
             'production_name'   => '测试商品',
-            'goods_info'=> '款式=;性别=;属性=',
+            'goods_info'=> '款式=xxx;性别=xxx;属性=xxx',
             'price'     => 0.01,
             'pay_id'    => $params['pay_id'],
             'pay_type'  => 1,
@@ -346,54 +354,89 @@ class Xpay extends Backend
 
         $this->orderModel->isUpdate(false)->save($data);
 
-        $data = [
-            'ticket' => time(),/*用来匹配请求*/
-            'service' => 'pay.xiangqian.wxjspay',
-            'version' => '2.0',/*版本号 默认是2.0*/
-            'sign_type' => 'MD5',/*签名方式，默认是md5*/
-            'mch_code' => $payInfo['mch_code'],/*商户号 享多多系统的门店编码*/
-            'timestamp' => date('YmdHis', time()),/*时间戳 发送请求的时间，格式"yyyyMMddHHmmss"*/
-            'sign' => '',/*签名*/
-            'body' => [
-                'orderNo' => $params['sn'],/*商户订单号 商户系统内部的订单号 ,32个字符内、 可包含字母,确保在商户系统唯一*/
-                'order_info' => 'test',/*商品描述*/
-                'total_amount' => 1,/*总金额，以分为单位，不允许包含任何字、符号*/
-                'mch_create_ip' => $this->request->ip(),/*订单生成的机器 IP*/
-                'notify_url' => 'http://back.dehub.com.cn/index.php/index/notify/xpayNotify',
-                'sub_appid' => 'wx092575bf6bc1636d',/*wx092575bf6bc1636d*/
-                'sub_openid' => $params['openid'],
-            ],
-        ];
-        //缓存当前申请支付的临时订单与本订单之前的关系
-        $newParams = $this->XpaySignParams($data, $payInfo['mch_key']);
-        $data['sign'] = $newParams;
-        //构建请求支付接口参数
-        $urlParams = str_replace('\\', '', json_encode($data, JSON_UNESCAPED_UNICODE));
-        //发起POST请求，获取订单信息
-        $result = $this->curlPostJson($urlParams, 'http://openapi.xiangqianpos.com/gateway');
-        //接收请求下单接口回来的数据
-        $newData = json_decode($result,true);
-        //计算下单接口返回过来数据的签名
-        $newParams1 = $this->XpaySignParams($newData,$payInfo['mch_key']);
-        //构建跳转收银台所需要的参数
-        $jsonData = [
-            'casher_id' => $newData['body']['casher_id'],
-            'mch_code'  => $payInfo['mch_code'],
-            'third_no'  => $params['sn'],
-            'sign'      => ''
-        ];
-        //
-        $cashSign = $this->XpaySignParams($jsonData,$payInfo['mch_key']);
-        //构建跳转的参数
-        $queryString = 'mch_code='.$payInfo['mch_code'].'&sign='.$cashSign.'&casher_id='.$newData['body']['casher_id'].'&third_no='.$params['sn'];
+        //开始进行支付操作。先判断是否已经下过订单
+        if (!Cache::has('x-'.$params['sn'])) {
+            $data = [
+                'ticket' => time(),/*用来匹配请求*/
+                'service' => 'pay.xiangqian.wxjspay',
+                'version' => '2.0',/*版本号 默认是2.0*/
+                'sign_type' => 'MD5',/*签名方式，默认是md5*/
+                'mch_code' => $payInfo['mch_code'],/*商户号 享多多系统的门店编码*/
+                'timestamp' => date('YmdHis', time()),/*时间戳 发送请求的时间，格式"yyyyMMddHHmmss"*/
+                'sign' => '',/*签名*/
+                'body' => [
+                    'orderNo' => $params['sn'],/*商户订单号 商户系统内部的订单号 ,32个字符内、 可包含字母,确保在商户系统唯一*/
+                    'order_info' => 'test',/*商品描述*/
+                    'total_amount' => 1,/*总金额，以分为单位，不允许包含任何字、符号*/
+                    'mch_create_ip' => $this->request->ip(),/*订单生成的机器 IP*/
+                    'notify_url' => 'http://back.dehub.com.cn/index.php/index/notify/xpayNotify',
+                    'sub_appid' => 'wx092575bf6bc1636d',/*wx092575bf6bc1636d*/
+                    'sub_openid' => $params['openid'],
+                ],
+            ];
+            //缓存当前申请支付的临时订单与本订单之前的关系
+            $newParams = $this->XpaySignParams($data, $payInfo['mch_key']);
+            $data['sign'] = $newParams;
+            //构建请求支付接口参数
+            $urlParams = str_replace('\\', '', json_encode($data, JSON_UNESCAPED_UNICODE));
+            //发起POST请求，获取订单信息
+            $result = $this->curlPostJson($urlParams, 'http://openapi.xiangqianpos.com/gateway');
+            /**
+             * 此处非常重要
+             * 缓存请求数据，避免重复请求，核心缓存功能，请求第一次下单成功后缓存好下单接口返回的数据。
+             * 默认情况，一次性请求成功，直接走此方法就完成了收银台的拉取操作。以及支付等功能，
+             * 如果说用户网络问题与手机卡，千万跳转支付时比较慢，其实是已经下单成功，只是没跳转收银台，这个时候，用户如果使用手机刷新页面功能，则会进行缓存请求
+             */
+            Cache::set('x-'.$params['sn'],$result,1800);
+            //接收请求下单接口回来的数据
+            $newData = json_decode($result,true);
+            //计算下单接口返回过来数据的签名
+            $newParams1 = $this->XpaySignParams($newData,$payInfo['mch_key']);
+            //构建跳转收银台所需要的参数
+            $jsonData = [
+                'casher_id' => $newData['body']['casher_id'],
+                'mch_code'  => $payInfo['mch_code'],
+                'third_no'  => $params['sn'],
+                'sign'      => ''
+            ];
+            //
+            $cashSign = $this->XpaySignParams($jsonData,$payInfo['mch_key']);
+            //构建跳转的参数
+            $queryString = 'mch_code='.$payInfo['mch_code'].'&sign='.$cashSign.'&casher_id='.$newData['body']['casher_id'].'&third_no='.$params['sn'];
 
-        // 验证下单接口的签名，如果签名没问题，返回JSON数据跳转收银台，如果有问题则不跳转
-        if ($newParams1 == $newData['sign']) {
-            //构建json数据
-            $url = 'https://open.xiangqianpos.com/wxJsPayV3/casher'.'?'.$queryString;
-            header('Location:'.$url);
+            // 验证下单接口的签名，如果签名没问题，返回JSON数据跳转收银台，如果有问题则不跳转
+            if ($newParams1 == $newData['sign']) {
+                //构建json数据
+                $url = 'https://open.xiangqianpos.com/wxJsPayV3/casher'.'?'.$queryString;
+                header('Location:'.$url);
+            } else {
+                return '';
+            }
         } else {
-            return '';
+            $result = Cache::get('x-'.$params['sn']);
+            $newData = json_decode($result,true);
+            //计算下单接口返回过来数据的签名
+            $newParams1 = $this->XpaySignParams($newData,$payInfo['mch_key']);
+            //构建跳转收银台所需要的参数
+            $jsonData = [
+                'casher_id' => $newData['body']['casher_id'],
+                'mch_code'  => $payInfo['mch_code'],
+                'third_no'  => $params['sn'],
+                'sign'      => ''
+            ];
+            //
+            $cashSign = $this->XpaySignParams($jsonData,$payInfo['mch_key']);
+            //构建跳转的参数
+            $queryString = 'mch_code='.$payInfo['mch_code'].'&sign='.$cashSign.'&casher_id='.$newData['body']['casher_id'].'&third_no='.$params['sn'];
+
+            // 验证下单接口的签名，如果签名没问题，返回JSON数据跳转收银台，如果有问题则不跳转
+            if ($newParams1 == $newData['sign']) {
+                //构建json数据
+                $url = 'https://open.xiangqianpos.com/wxJsPayV3/casher'.'?'.$queryString;
+                header('Location:'.$url);
+            } else {
+                return '';
+            }
         }
     }
 
