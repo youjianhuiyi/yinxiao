90ff40611ff24f4fbeab354153ae5dc2c088de0d
yulinzhihou <yulinzhihou@163.com>
2020-05-08 23:36:17 +0800

    5-8 完成新文案的调整-1

--- database/migrations/20200508050255_summary_analysis_create.php
+++ database/migrations/20200508050255_summary_analysis_create.php
@@ -39,7 +39,7 @@ class SummaryAnalysisCreate extends Migrator
             ->addColumn('check_code','string',['limit'=>32,'null'=>false,'default'=>'','comment'=>'推广码'])
             ->addColumn('order_sn','string',['limit'=>32,'null'=>false,'default'=>0,'comment'=>'订单号'])
             ->addColumn('type','integer',['limit'=>MysqlAdapter::INT_TINY,'signed'=>false,'null'=>false,'default'=>0,'comment'=>'数据类型，0=订单，1=订单数量，2=支付，3=支付数量'])
-            ->addColumn('data','text',['null'=>false,'default'=>'','comment'=>'原始数据'])
+            ->addColumn('data','text',['null'=>false,'comment'=>'原始数据'])
             ->addColumn('num','integer',['limit'=>10,'signed'=>false,'null'=>false,'default'=>0,'comment'=>'订单商品数量'])
             ->addColumn('createtime','integer',['limit'=>10,'signed'=>false,'null'=>false,'default'=>0,'comment'=>'增加时间'])
             ->addColumn('updatetime','integer',['limit'=>10,'signed'=>false,'null'=>false,'default'=>0,'comment'=>'更新时间'])
