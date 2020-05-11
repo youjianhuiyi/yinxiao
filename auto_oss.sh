8bec6a7dbe9f436a5da20800a13f35260dac8c41
yulinzhihou <yulinzhihou@163.com>
2020-05-11 18:59:07 +0800

    5-11 报表展示调整

---
+++ auto_oss.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+host="oss-cn-hangzhou.aliyuncs.com"
+bucket="order-backup"
+Id="LTAI4G8Z9Ng91NPnu4sNMnmw"
+Key="zGXnq5sJIoBUary7oHDYq7y2wkuQV1"
+# 参数1，PUT：上传，GET：下载
+method="PUT"
+# 参数2，上传时为本地源文件路径，下载时为oss源文件路径
+source=$1
+# 参数3，上传时为OSS目标文件路径，下载时为本地目标文件路径
+dest=$2
+
+osshost=$bucket.$host
+
+#校验上传目标路径
+if test -z "$dest"
+then
+    dest=$source
+fi
+
+echo "method:"$method
+echo "source:"$source
+echo "dest:"$dest
+
+#校验参数是否为空
+if test -z "$method" || test -z "$source" || test -z "$dest"
+then
+    echo $0 put localfile objectname
+    echo $0 get objectname localfile
+    exit -1
+fi
+
+if [ "${method}"x = "PUT"x ]
+then
+    resource="/${bucket}/${dest}"
+    contentType=`file -ib ${source} |awk -F ";" '{print $1}'`
+    dateValue="`TZ=GMT date +'%a, %d %b %Y %H:%M:%S GMT'`"
+    stringToSign="${method}\n\n${contentType}\n${dateValue}\n${resource}"
+    signature=`echo -en $stringToSign | openssl sha1 -hmac ${Key} -binary | base64`
+    echo $stringToSign
+    echo $signature
+    url=http://${osshost}/${dest}
+    echo "upload ${source} to ${url}"
+    curl -i -q -X PUT -T "${source}" \
+      -H "Host: ${osshost}" \
+      -H "Date: ${dateValue}" \
+      -H "Content-Type: ${contentType}" \
+      -H "Authorization: OSS ${Id}:${signature}" \
+      ${url}
+else
+    resource="/${bucket}/${source}"
+    contentType=""
+    dateValue="`TZ=GMT date +'%a, %d %b %Y %H:%M:%S GMT'`"
+    stringToSign="${method}\n\n${contentType}\n${dateValue}\n${resource}"
+    signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${Key} -binary | base64`
+    url=http://${osshost}/${source}
+    echo "download ${url} to ${dest}"
+    curl --create-dirs \
+      -H "Host: ${osshost}" \
+      -H "Date: ${dateValue}" \
+      -H "Content-Type: ${contentType}" \
+      -H "Authorization: OSS ${Id}:${signature}" \
+      ${url} -o ${dest}
+fi
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -3,42 +3,42 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'template', 'echarts'
     var data = Config.data;
     var Controller = {
         index: function () {
-            // 基于准备好的dom，初始化echarts实例
-            var myChart = Echarts.init(document.getElementById('echart'), 'walden');
-
-            // 指定图表的配置项和数据
-            var option = {
-                legend: {
-                    data: ['入账金额','浏览数','订单数量','订单商品数量','支付成功订单','支付成功商品数量']
-                },
-                xAxis: {type: 'category',data:['入账金额','浏览数','订单数量','订单商品数量','支付成功订单','支付成功商品数量']},
-                yAxis: {type: 'value'},
-                grid: {},
-                tooltip: {
-                    trigger: 'axis',
-                    showContent: true
-                },
-                series: [
-                    {
-                        "name": "",
-                        "type": "bar",
-                        "showBackground": true,
-                        "backgroundStyle": {
-                            "color": "rgba(220, 220, 220, 0.8)"
-                        },
-                        "data":[data.pay_total,Math.ceil(data.visit*0.9),data.order_count,data.order_nums,data.pay_done,data.pay_done_nums]
-                    }
-                ]};
+            // // 基于准备好的dom，初始化echarts实例
+            // var myChart = Echarts.init(document.getElementById('echart'), 'walden');
+            //
+            // // 指定图表的配置项和数据
+            // var option = {
+            //     legend: {
+            //         data: ['入账金额','浏览数','订单数量','订单商品数量','支付成功订单','支付成功商品数量']
+            //     },
+            //     xAxis: {type: 'category',data:['入账金额','浏览数','订单数量','订单商品数量','支付成功订单','支付成功商品数量']},
+            //     yAxis: {type: 'value'},
+            //     grid: {},
+            //     tooltip: {
+            //         trigger: 'axis',
+            //         showContent: true
+            //     },
+            //     series: [
+            //         {
+            //             "name": "",
+            //             "type": "bar",
+            //             "showBackground": true,
+            //             "backgroundStyle": {
+            //                 "color": "rgba(220, 220, 220, 0.8)"
+            //             },
+            //             "data":[data.pay_total,Math.ceil(data.visit*0.9),data.order_count,data.order_nums,data.pay_done,data.pay_done_nums]
+            //         }
+            //     ]};
 
             //动态添加数据，可以通过Ajax获取数据然后填充
             setInterval(function () {
                 window.location.reload()
             }, 60000);
-            $(window).resize(function () {
-                myChart.resize();
-            });
+            // $(window).resize(function () {
+            //     myChart.resize();
+            // });
             // 使用刚指定的配置项和数据显示图表。
-            myChart.setOption(option);
+            // myChart.setOption(option);
         }
     };
     return Controller;
