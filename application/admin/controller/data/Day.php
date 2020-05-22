5fe4cc06d2b51ab4dc7f07add7c4e6caaa577f21
yulinzhihou <yulinzhihou@gmail.com>
2020-05-22 15:29:04 +0800

    5-22 优化报表数据算法，添加各数据库日期字段-2

--- application/admin/controller/data/Day.php
+++ application/admin/controller/data/Day.php
@@ -449,7 +449,7 @@ class Day extends Backend
         $this->assign('today_total',$todayTotal);/*当天数据汇总*/
         $this->assign('data',$newArr);
         $this->assign('date',$date ? $date : date('m-d',time()));
-        $this->assign('select_data',$newSelectData);/*查询数据*/
+        $this->assign('select_data',$newSelectData ? $newSelectData : [date('m-d',time())]);/*查询数据*/
         $this->assign('zz_data',$zzData);/*组长数据*/
         $this->assign('yg_data',$ygData);/*员工数据*/
         $this->assign('td_data',$tdData);/*团队数据*/
