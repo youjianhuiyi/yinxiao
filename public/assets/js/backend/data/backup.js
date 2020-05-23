fb39213f56863b2ad2722c9b76590289e9800a51
yulinzhihou <yulinzhihou@gmail.com>
2020-05-23 14:36:09 +0800

    5-23 优化前端

--- public/assets/js/backend/data/backup.js
+++ public/assets/js/backend/data/backup.js
@@ -21,18 +21,36 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
                 pk: 'id',
                 sortName: 'id',
+                search:false,
+
                 columns: [
                     [
                         {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'team_id', title: __('Team_id')},
-                        {field: 'admin_id', title: __('Admin_id')},
-                        {field: 'file_name', title: __('File_name')},
-                        {field: 'data_count', title: __('Data_count')},
-                        {field: 'date', title: __('Date')},
-                        {field: 'status', title: __('Status')},
+                        {
+                            title:'序号',
+                            operate:false,
+                            sortable:false,
+                            formatter:function(value,row,index){
+                                return index+1;
+                            }
+                        },
+                        {field: 'id', title: __('Id'),operate: false,visible: false},
+                        {field: 'team_id', title: __('Team_id'),operate: false,visible: false},
+                        {field: 'team_id_text', title: __('Team_id'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
+                        {field: 'admin_id', title: __('Admin_id'),operate: false,visible: false},
+                        {field: 'admin_id_text', title: __('Admin_id'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
+                        {field: 'file_name', title: __('File_name'),operate: 'LIKE %...%', placeholder: '模糊搜索，*表示任意字符'},
+                        {field: 'data_count', title: __('Data_count'),operate: false},
+                        {field: 'date', title: __('Date'),operate: false},
+                        {field: 'status', title: __('Status'),searchList: {
+                                "0":"失败",
+                                "1":"成功",
+                        },formatter:function (value,row,index) {
+                            if (value == 0){return '<span class="label bg-red">失败</span>';}
+                            if (value == 1){return '<span class="label bg-green">成功</span>';}
+                        }},
                         {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime,visible:false},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
